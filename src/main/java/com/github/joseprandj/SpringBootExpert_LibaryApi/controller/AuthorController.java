5e7e46269f4e15bd9dfbbf709435ce042bf702fb
Jose Pereira de Andrade Junior <jose.junior@doalti.corp>
2026-01-06 16:38:52 -0300

    Atualizada versão do spring boot e adicionado o spring web

---
+++ src/main/java/com/github/joseprandj/SpringBootExpert_LibaryApi/controller/AuthorController.java
@@ -0,0 +1,7 @@
+package com.github.joseprandj.SpringBootExpert_LibaryApi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+}
